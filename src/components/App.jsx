cef1dd248feef0741f003a619d3dddf3a9ac6051
Rodion <89980@ukr.net>
2025-03-03 12:53:36 +0200

    hw-react-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import UserProfile from "./UserProfile/UserProfile";
 import UserData from "../UserData.json";
 import friends from "../friends.json";
-import FriendListItem from "./FriendsList/FriendsList";
+import FriendList from "./FriendsList/FriendsList";
 import transactions from "../transactions.json";
 import TransactionList from "./Transactions/TransactionHistory";
 console.log(UserData);
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <>
       <UserProfile user={UserData} />
-      <FriendListItem friends={friends} />
+      <FriendList friends={friends} />
       <TransactionList items={transactions} />
     </>
   );
